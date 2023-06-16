937595e60b438a3e045ff816e9138bbf1b27eb25
Noelle <noelle.maingi@student.moringaschool.com>
2023-06-16 12:17:35 +0300

    Speed detector function added

--- index.js
+++ index.js
@@ -28,3 +28,25 @@ function gradeGenerator() {
 }
 
 gradeGenerator();
+
+function speedDetector() {
+  let speed = prompt("Input the speed of your car:");
+  let speedlimit = 70;
+  let totalDemerits = 0;
+
+  if (speed > 70) {
+    let overspeeding = speed - speedlimit;
+    let demerits = Math.floor(overspeeding / 5);
+
+    totalDemerits += demerits;
+    alert(`Points: ${totalDemerits} `);
+
+    if (totalDemerits >= 12) {
+      alert("License suspended!");
+    }
+  } else {
+    alert("Ok");
+  }
+}
+
+speedDetector();
